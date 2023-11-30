f25bcc91606305805030be223e3864323235a5a6
Christofer Bertonha <christoferbertonha@gmail.com>
2023-11-30 19:29:20 +0100

    Move back to AXUM

--- src/api.rs
+++ src/api.rs
@@ -1,20 +1,22 @@
-use actix_web::{get, post, web, Responder};
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+use tower_http::trace::TraceLayer;
 
 use crate::command::{convert, process_command};
 use crate::telegram::{InlineQueryResult, RequestType, TelegramRequest, TelegramResponse};
 
-#[get("/")]
 async fn welcome() -> &'static str {
     "<h1>Welcome!</h1>"
 }
 
-#[post("/")]
-async fn receive_message(web::Json(payload): web::Json<TelegramRequest>) -> impl Responder {
+async fn receive_message(Json(payload): Json<TelegramRequest>) -> Json<Option<TelegramResponse>> {
     let response = match RequestType::from_request(payload) {
         RequestType::Message(message) => {
             if let Some(via_bot) = message.via_bot {
                 if via_bot.is_bot {
-                    return web::Json(None);
+                    return Json(None);
                 }
             }
 
@@ -47,57 +49,83 @@ async fn receive_message(web::Json(payload): web::Json<TelegramRequest>) -> impl
         RequestType::Unknown => None,
     };
 
-    web::Json(response)
+    Json(response)
+}
+
+pub fn app() -> Router {
+    Router::new()
+        .route("/", get(welcome))
+        .route("/", post(receive_message))
+        .layer(TraceLayer::new_for_http())
 }
 
 #[cfg(test)]
 mod tests {
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use serde_json::json;
+    use tower::ServiceExt;
+
     use super::*;
 
-    use actix_web::{
-        http::{header::ContentType, Method},
-        test, App,
-    };
-
-    #[actix_web::test]
+    #[tokio::test]
     async fn test_welcome() {
-        let app = test::init_service(App::new().service(welcome)).await;
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = test::call_service(&app, req).await;
-        assert!(resp.status().is_success());
-        assert_eq!(test::read_body(resp).await, "<h1>Welcome!</h1>");
+        let app = app();
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"<h1>Welcome!</h1>");
     }
 
-    #[actix_web::test]
+    #[tokio::test]
     async fn test_receive_message() {
-        let app = test::init_service(App::new().service(receive_message)).await;
-        let req = test::TestRequest::default()
-            .method(Method::POST)
-            .insert_header(ContentType::json())
-            .set_json(serde_json::json!(
-                {
-                    "update_id": 123,
-                    "message": {
-                        "message_id": 123,
-                        "text": "/start",
-                        "date": 123,
-                        "from": {
-                            "id": 123,
-                            "is_bot": false,
-                            "first_name": "John",
-                        },
-                        "chat": {
-                            "id": 123,
-                            "type": "private",
-                        },
-                    }
-                }
-            ))
-            .to_request();
-        let resp = test::call_service(&app, req).await;
-        assert!(resp.status().is_success());
+        let app = app();
 
-        let data: TelegramResponse = test::read_body_json(resp).await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        json!(
+                            {
+                                "update_id": 123,
+                                "message": {
+                                    "message_id": 123,
+                                    "text": "/start",
+                                    "date": 123,
+                                    "from": {
+                                        "id": 123,
+                                        "is_bot": false,
+                                        "first_name": "John",
+                                    },
+                                    "chat": {
+                                        "id": 123,
+                                        "type": "private",
+                                    },
+                                }
+                            }
+                        )
+                        .to_string(),
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let data: TelegramResponse = serde_json::from_slice(&body).unwrap();
+
         assert_eq!(data.chat_id, Some(123));
         assert_eq!(data.method, "sendMessage".to_string());
         assert_eq!(data.text, Some("Welcome!\n\nCommands accepted:\n/start\n/now <timezone>\n/convert <time> <source_timezone> <target_timezone>".into()));
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use actix_web::{App, HttpServer};
-
-use crate::api::{receive_message, welcome};
+use crate::api::app;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod api;
 mod command;
 mod telegram;
 mod time;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(welcome).service(receive_message))
-        .bind(("0.0.0.0", 3000))?
-        .run()
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "chronosbot=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app()).await.unwrap();
 }
